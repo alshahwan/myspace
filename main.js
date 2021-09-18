5370f4620846311f7f60da9d968dbe380cccecc8
Mohammad Alshahwan <shahwanoo@live.com>
2021-09-19 01:19:27 +0200

    started wokring with js to make the side list more active

--- main.js
+++ main.js
@@ -1,4 +1,22 @@
+// Bootsrap popover effect
 var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
 var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
   return new bootstrap.Popover(popoverTriggerEl)
 })
+
+// Selectors
+const LeftMenuItems = document.querySelectorAll(".menu_item");
+const ActiveListItem = document.querySelector(".active");
+
+// Functions
+function ChangeActiveListItem(item) {
+  ActiveListItem.classList.remove("active");
+  item.classList.add("active");
+};
+
+// Listeners and Executors
+LeftMenuItems.forEach((item) => {
+  item.addEventListener("click", (event) => {
+    ChangeActiveListItem(item);
+  });
+});
